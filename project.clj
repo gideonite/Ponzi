9015f6f10ee79ee52623bc9a540d11799525f90f
Gideon Dresdner <gideond@gmail.com>
2013-09-30 19:11:26 -0400

    init

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject interpreter "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1806"]]
+  :plugins      [[lein-cljsbuild "0.3.2"]
+                 [lein-ring "0.8.7"]]
+  :source-paths ["src/clj"]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src-cljs"],
+     :id "main",
+     :compiler
+     {:pretty-print true,
+      :output-to "resources/public/js/cljs.js",
+      :optimizations :simple}}]})
