e660d6b966e7a2d0c7d7ee4a2561828455a0fff0
Gideon Dresdner <gideond@gmail.com>
2013-12-13 17:21:48 -0500

    Arithmetic calculator cleanup for Hacker School presentation. Would be nice to have a calc that uses an explicit stack, i.e. tail-recursion.

--- src/interpreter/arithmetic.clj
+++ src/interpreter/arithmetic.clj
@@ -1,37 +1,42 @@
-(ns interpreter.arithmetic)
-
-(def lookup
-  {'+ +
-   '- -
-   '/ /
-   '* *})
-
-(def primitive-procs (set (vals lookup)))
+(ns interpreter.arithmetic
+  (:require [clojure.core.match :refer [match]]))
 
 (defn halt [val]
   (println "HALT!" val)
   val)
 
-(defn evaluate
-  [exp k]
-  (cond
-    (number? exp) (k exp)
-    ((complement nil?) (primitive-procs exp)) (k exp)
-    ((complement nil?) (lookup exp)) (k (lookup exp))
-    :else (let [[f left right] exp]
-            (evaluate left (fn [l]    ;; push left
-                             (evaluate right (fn [r] ;; push right
-                                               (evaluate f (fn [fun] ;; push f
-                                                             (evaluate (fun l r) (fn [v] (k v)))))))))))) ;; push k
+(defn eval-stack
+  [exp]
+  (match [exp]
+         [(_ :guard number?)] exp
+         [(_ :guard #{'+ '- '/ '*})] (eval exp)
+         [([ op left right] :seq)] ((eval-stack op) (eval-stack left) (eval-stack right))
+         :else "ERROR!"))
 
 (comment
-  (evaluate '1 halt)
-  (evaluate '+ halt)
-  (evaluate '(+ 1 1) halt)
-  (evaluate '(- 1 1) halt)
-  (evaluate '(/ 1 2) halt)
-  (evaluate '(* 2 1) halt)
+  (eval-stack 1)
+  (eval-stack '+)
+  (eval-stack '(+ 1 1))
+  (eval-stack '(+ 1 (+ 2 3)))
+  (eval-stack '(+ 1 (/ 2 3))))
 
-  (evaluate '(+ (+ 1 2) (+ 3 (+ 4 5))) halt)
-  )
+(defn eval-cont
+  [exp k]
+  (println exp)
+  (match [exp]
+         [(_ :guard number?)] (k exp)
+         [(_ :guard #{'+ '- '/ '*})] (k (eval exp))
+         [([op left right] :seq)] (eval-cont op (fn [op]
+                                                   (eval-cont left (fn [left]
+                                                                     (eval-cont right
+                                                                                (fn [right] (k (op left right))))))))
+         :else "ERROR!"))
 
+(comment
+  (eval-cont '1 halt)
+  (eval-cont '+ halt)
+  (eval-cont '(+ 1 1) halt)
+  (eval-cont '(- 1 1) halt)
+  (eval-cont '(/ 1 2) halt)
+  (eval-cont '(* 2 1) halt)
+  (eval-cont '(+ (+ 1 2) (+ 3 (+ 4 5))) halt))
